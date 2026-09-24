bbce6ef566fd93121d77da53bb42fc34d7319475
ceci869 <aine.s@escolar.ifrn.edu.br>
2026-09-24 20:41:49 -0300

    modificação no service worker

--- sw.js
+++ sw.js
@@ -10,7 +10,15 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.resquest, networkResponse.clone());
+          return networkResponse;
+        });
+      });
     })
   );
 });
